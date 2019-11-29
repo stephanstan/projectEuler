28bab91c8e6057c26f2392404b72858ee403a1a5
Stephan Stan <stephanstan@gmail.com>
2019-11-28 22:20:39 -0700

    other euler questions

--- src/main/java/stephanstan/euler/Fraction.java
+++ src/main/java/stephanstan/euler/Fraction.java
@@ -1,4 +1,31 @@
 package stephanstan.euler;
 
+/**
+ * https://www.geeksforgeeks.org/comparator-interface-java/
+ */
 public class Fraction {
+
+    private int numerator;
+    private int denominator;
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public void setDenominator(int denominator) {
+        this.denominator = denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
 }
--- src/test/java/stephanstan/euler/FractionTest.java
+++ src/test/java/stephanstan/euler/FractionTest.java
@@ -1,4 +1,53 @@
 package stephanstan.euler;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
 public class FractionTest {
+
+    @Test
+    void howManyFractionsWouldYouLike(){
+        Fraction frac;
+        int i,j;
+        int control=8;
+
+        for(j=control;j>1;j--){
+            for(i=control-1;i>0;i--){
+                if(i<j){
+                    frac=new Fraction(i,j);
+                    System.out.println(frac.getNumerator()+" / "+frac.getDenominator());
+                }
+            }
+        }
+    }
+
+
+    @Disabled
+    @Test
+    void basicObjectDesc() {
+        Fraction frac;
+        int i,j;
+
+        for (j = 8; j > 1; j--) {
+            for (i = 7; i > 0; i--) {
+                if (i < j) {
+                    frac = new Fraction(i, j);
+                    System.out.println(frac.getNumerator() + " / " + frac.getDenominator());
+                }
+            }
+        }
+    }
+
+    @Disabled
+    @Test
+    void basicObjectAsc() {
+        Fraction frac;
+        int i;
+
+        for ( i = 1; i < 8; i++) {
+            frac = new Fraction(i,8);
+            System.out.println(frac.getNumerator() +" / " + frac.getDenominator());
+        }
+    }
 }
